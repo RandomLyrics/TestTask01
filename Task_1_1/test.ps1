aa817d9cb09979990a9d1666c56a34615db1eebd
Marduk14 <randomlyricsmusic@gmail.com>
2019-05-29 19:24:54 +0200

    ps1

--- Task_1_1/test.ps1
+++ Task_1_1/test.ps1
@@ -1,3 +1,23 @@
-Get-Process `
-| Where-Object {$_.company -Notlike ‘*Microsoft*’}`
-| Format-Table ProcessName, Company -auto
+#Requires -Version 3.0
+
+Param(
+    [string] 
+    <# [Parameter(Mandatory=$true)]  #>
+    $ResourceGroupName = 'TEST_GROUP',
+
+    [string[]] 
+    [Parameter(ValueFromRemainingArguments=$true)]
+    $ResourceTypes = @('Microsoft.Web/serverFarms', 'Microsoft.Web/sites')
+)
+
+<# Connect-AzureRmAccount #>
+
+<# NOT WORKING TILL ver 6.0.0 #>
+<# Get-AzureRmResource -ResourceGroupName $ResourceGroupName #>
+
+$RS = Get-AzureRmResource -ODataQuery "`$filter=resourcegroup eq '$ResourceGroupName'"
+foreach ($r in $RS) {
+    if ($ResourceTypes.Contains($r.ResourceType)) {
+        Remove-AzureRmResource -ResourceId $r.ResourceId
+    }
+}
