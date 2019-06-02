b0e694bdc3873722c1651514fdc2ba05fc6074b8
Marduk14 <randomlyricsmusic@gmail.com>
2019-06-02 18:37:57 +0200

    fixy

--- Task_1_3/Test01/wwwroot/js/site.js
+++ Task_1_3/Test01/wwwroot/js/site.js
@@ -2,4 +2,46 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-console.log('sssss');
+
+//GLOBALS
+const _DateFormat = 'DD.MM.YYYY hh:mm';
+const _DateFormatToBack = 'MM-DD-YYYY';
+const _DateFormatPL = 'DD.MM.YYYY';
+
+// AJAX API
+var _ajaxJson = (function () {
+    'use strict';
+
+    function BaseCall(url, rType, obj) {
+        return $.ajax({
+            url: url,
+            type: rType,
+            data: JSON.stringify(obj),
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (result) { },
+            error: function (errormessage) {
+                alert(errormessage.responseText);
+            }
+        });
+    }
+
+    return {
+        get: function (url) { return BaseCall(url, "GET"); },
+        post: function (url, obj) { return BaseCall(url, "POST", obj); },
+        put: function (url, obj) { return BaseCall(url, "PUT", obj); },
+        delete: function (url) { return BaseCall(url, "DELETE"); }
+    };
+}());
+
+
+// DATES
+function toDateFormat(date) {
+    date = date + 'Z';
+    date = moment(date).local().format(_DateFormat);
+    return date;
+}
+function convertDateToUTC(date) {
+    date = moment(date, _DateFormat);
+    return date;
+}
